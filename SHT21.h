a44f8f2a4b782823df0b7f4c4360c9f7eeeb4ee7
fawks03 <fawks03@gmail.com>
2017-12-30 14:59:29 +0100

    Update SHT21.h

--- SHT21.h
+++ SHT21.h
@@ -5,15 +5,16 @@
   github@tradewire.de
   23-5-2016
   https://github.com/markbeee/SHT21
-  
+
   With credits to:
-  
+
   HTU21D Humidity Sensor Library
   By: Nathan Seidle
   SparkFun Electronics
   Date: September 22nd, 2013
   License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
 
+29/12/2017 edited by fawks03 - async reading
 */
 
 #ifndef SHT21_H
@@ -28,18 +29,36 @@
 #define TRIGGER_HUMD_MEASURE_NOHOLD  0xF5
 
 class SHT21 {
-   
-public:
-  SHT21();
-	
-  void begin();
-  float getHumidity(void);
-  float getTemperature(void);
-  	
-private:
-  
-  uint16_t readSHT21(uint8_t command);
-  
+
+  public:
+    SHT21();
+
+    void begin();
+    void update();
+    float getHumidity(void);
+    float getTemperature(void);
+    void setTempWait (int val);
+    void setHumdWait (int val);
+    void setHold (bool val);
+	void setInterval(int val);
+
+  private:
+    uint16_t readSHT21(uint8_t command);
+    int tempWait = 48;
+    int humdWait = 16;
+
+    float temp = 0;
+    float humd = 0;
+
+
+    bool hold = false;
+    int state = 0;
+    long waitUntil;
+    bool currentRead = false; //false = temp, true = humd
+
+    int timeout = 100;
+	int refreshInterval = 250;
+	long nextRefresh;
 };
 
 #endif
