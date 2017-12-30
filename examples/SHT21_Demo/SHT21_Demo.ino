86bf83f3e232e1cb52a6aab286dd96c02b6f8ca3
fawks03 <fawks03@gmail.com>
2017-12-30 15:17:14 +0100

    Update SHT21_Demo.ino

--- examples/SHT21_Demo/SHT21_Demo.ino
+++ examples/SHT21_Demo/SHT21_Demo.ino
@@ -2,7 +2,7 @@
 *  SHT21_Demo
 *
 *  An example sketch that reads the sensor and prints the
-*  relative humidity to the serial port
+*  temperature and relative humidity to the serial port
 * 
 ***************************************************************/
 
@@ -13,16 +13,19 @@ SHT21 SHT21;
 
 void setup()
 {
-  SHT21.begin();
+  SHT21.setHold(true);    //markbeee's version
+  SHT21.setTempWait(48);  //default value
+  SHT21.setHumdWait(16);  //default value
+  SHT21.begin();          //don't use SHT21.update() if setHold(true)
   Serial.begin(9600);
 }
 
 void loop()
 {
   Serial.print("Humidity(%RH): ");
-  Serial.print(SHT21.getHumidity());
+  Serial.print(SHT21.getHumidity());      //waits until sensors read finished and returns value
   Serial.print("     Temperature(C): ");
-  Serial.println(SHT21.getTemperature());
+  Serial.println(SHT21.getTemperature()); //waits until sensors read finished and returns value
   
   delay(1000);
 }
